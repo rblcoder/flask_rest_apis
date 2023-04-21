9677ee32c3d0cfa59b240fed8ad89869f1e78163
rblcoder <35212670+rblcoder@users.noreply.github.com>
2023-04-21 18:52:01 +0530

    changes according to tutorial

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 
 from flask import Flask, render_template, request
-import requests
+
 from werkzeug.utils import secure_filename
-UPLOAD_FOLDER = '/home/ch1/Documents/uploads/'
+
+UPLOAD_FOLDER = 'uploads/'
 
 Headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:28.0) Gecko/20100101 Firefox/28.0'}
 
@@ -18,13 +19,12 @@ def upload_file():
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def process_file():
-
     if request.method == 'POST':
         f = request.files['file']
         s_secure_filename = secure_filename(f.filename)
-        if s_secure_filename.endswith('.csv'):
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], s_secure_filename))
-            return 'file processed successfully'
+        # if s_secure_filename.endswith('.csv'):
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], s_secure_filename))
+        return 'file processed successfully'
 
 
 if __name__ == "__main__":

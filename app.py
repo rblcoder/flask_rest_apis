1731a94d57a157ce869564e1bbe8f40c17ece352
Rupa Lahiri <rupacoach@gmail.com>
2023-10-03 16:40:21 +0530

    Load yaml file

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import os
 from flask import Flask
+import yaml
+import pprint
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    # name = os.environ.get("NAME", "World")
+    # return "Hello {}!".format(name)
+
+    with open('.github/workflows/changed-files.yml', 'r') as file:
+        load_yaml = yaml.safe_load(file)
+        print(type(load_yaml))
+        result_yaml = pprint.pformat(load_yaml)
+        print(result_yaml)
+        return str(load_yaml)
 
 
 if __name__ == "__main__":
